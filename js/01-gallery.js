0bab7f40047a04f6e33e64cb3e669ad822848fc6
Ihor <ihorrykun@gmail.com>
2022-08-28 22:33:56 +0300

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,13 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const galeryEl = document.querySelector('ul.gallery');
+console.log(galeryEl);
+
+function creatListItem(item) {
+    return galleryItems.map(item => {
+        console.log(item);
+    });
+}
