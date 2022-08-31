5965821b4fa34b0507e5c0cb3096a240401d9b73
Ihor <ihorrykun@gmail.com>
2022-08-31 17:06:43 +0300

    2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,57 +1,64 @@
-// import { galleryItems } from './gallery-items.js';
-// // Change code below this line
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
 
-// console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+//  !   Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї.
+
+function galleryImg(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+        <div class="gallery__item">
+  <a class="gallery__link" 
+  href="large-image.jpg">
+    <img class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>
+`;
+    })
+    .join("");
+}
+
+//      !Реалізація делегування на div.gallery і отримання url великого зображення     
+
+const imgMarkup = galleryImg(galleryItems); 
+gallery.insertAdjacentHTML("beforeend", imgMarkup);
+gallery.addEventListener('click', (e) => {
+  e.preventDefault();
+  const isGalleryEl = e.target.classList.contains("gallery__image");
+
+  if (!isGalleryEl) {
+    return;
+  }
+
+  //  !   Реалізація відкриття модального вікна
+
+  const swatchUrlEl = e.target.dataset.source;
+
+  //  !   Заміна значення атрибута src елемента <img> в модальному вікні перед відкриттям   
+  
+    const instance = basicLightbox.create(
+    `<img class="modal__image" src="${swatchUrlEl}"/>`
+  );
+    instance.show();
+    
+    //  !   Створюємо функцію для закриття модального вікна по Esc
+
+    window.addEventListener('keydown', (e) => {
+        const closeEsc = 'Escape';
+        if (e.code === closeEsc) {
+            instance.close();
+        }
+    });
+
+    
+});
 
 
-// function galleryImg(galleryItems) {
-   
-//     return galleryItems.map(({ preview, original, description }) => {
-//         return `<div class="gallery__item">
-//   <a class="gallery__link" href="${original}">
-//     <img
-//       class="gallery__image"
-//       src="${preview}"
-//       data-source="${original}"
-//       alt="${description}"
-//     />
-//   </a>
-// </div>`;
-//       })
-//         .join("");
-
-// }
-
-// const galleryItem = document.querySelector(".gallery");
-// const imgMarup = galleryImg(galleryItems); 
-// galleryItem.insertAdjacentHTML("beforeend", imgMarup);
-
-
-// const createLightboxInstance = (e) => {
-//     const targetImgUrl = e.target.dataset.source;
-//     const instance = basicLightbox.create (`
-//     <img src="${targetImgUrl}" width="800", height="600">`);
-
-
-//     instance.show(
-//         document.addEventListener('keydown', (e) => {
-//             if (e.key && e.code === "escape") {
-//                 instance.close();
-//             }
-//         })
-//     );
-// };
-
-
-// const onClickGallery = (e) => {
-//   if (e.target.nodeName !== "IMG") {
-//     return;
-//   }
-
-//   createLightboxInstance(e);
-//   e.preventDefault();
-// };
-
-// imgMarup.addEventListener("click", onClickGallery);
 
 
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,32 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const gallery = document.querySelector(".gallery");
+
+function galleryImg(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+  <a class="gallery__link" 
+  href="${original}">
+    <img class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+`;
+    })
+    .join("");
+}
+
+gallery.insertAdjacentHTML("beforeend", galleryImg(galleryItems));
+
+var lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+  captionPosition: "bottom",
+});
+
+console.log(gallery);
