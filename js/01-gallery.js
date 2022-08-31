154abdab89b851c90a24b1d495f0a64cc77810f8
Ihor <ihorrykun@gmail.com>
2022-08-31 11:36:41 +0300

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,49 +1,57 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+// import { galleryItems } from './gallery-items.js';
+// // Change code below this line
 
-console.log(galleryItems);
-
-
-const galleryItem = document.querySelector(".gallery");
-
-function galleryImg(galleryItems) {
-    return galleryItems.map(({ preview, original, description }) => {
-        return `<div class="gallery__item">
-  <a class="gallery__link" href="${original}">
-    <img
-      class="gallery__image"
-      src="${preview}"
-      data-source="large-image.jpg"
-      alt="${description}"
-    />
-  </a>
-</div>`;
-  }).join('');
-}
-
-galleryItem.insertAdjacentHTML("beforeend", galleryImg(galleryItems));
+// console.log(galleryItems);
 
 
+// function galleryImg(galleryItems) {
+   
+//     return galleryItems.map(({ preview, original, description }) => {
+//         return `<div class="gallery__item">
+//   <a class="gallery__link" href="${original}">
+//     <img
+//       class="gallery__image"
+//       src="${preview}"
+//       data-source="${original}"
+//       alt="${description}"
+//     />
+//   </a>
+// </div>`;
+//       })
+//         .join("");
+
+// }
+
+// const galleryItem = document.querySelector(".gallery");
+// const imgMarup = galleryImg(galleryItems); 
+// galleryItem.insertAdjacentHTML("beforeend", imgMarup);
 
 
+// const createLightboxInstance = (e) => {
+//     const targetImgUrl = e.target.dataset.source;
+//     const instance = basicLightbox.create (`
+//     <img src="${targetImgUrl}" width="800", height="600">`);
 
 
+//     instance.show(
+//         document.addEventListener('keydown', (e) => {
+//             if (e.key && e.code === "escape") {
+//                 instance.close();
+//             }
+//         })
+//     );
+// };
 
 
+// const onClickGallery = (e) => {
+//   if (e.target.nodeName !== "IMG") {
+//     return;
+//   }
 
+//   createLightboxInstance(e);
+//   e.preventDefault();
+// };
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+// imgMarup.addEventListener("click", onClickGallery);
 
 
